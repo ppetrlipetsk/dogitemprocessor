e06949f108d161e8993072acc7fb5db1133ecea2
ppetrlipetsk <ppetrlipetsk@yandex.ru>
2020-11-20 13:37:04 +0300

    20.11.2020 init commit

---
+++ src/main/java/com/ppsdevelopment/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.ppsdevelopment.controller;
+
+import com.ppsdevelopment.domain.Role;
+import com.ppsdevelopment.domain.User;
+import com.ppsdevelopment.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        model.put("message","User exists!");
+        if (userFromDb != null) {
+            model.put("message","Пользователь существует!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/ppsdevelopment/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package com.ppsdevelopment.repos;
+
+
+import com.ppsdevelopment.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
